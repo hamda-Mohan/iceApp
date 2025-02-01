430c9cfcd07dc92b1268354e26c57e4be794cffb
RAHMA-SAID-afk <rahmasaid745@gmail.com>
2025-02-01 04:51:31 -0800

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,1 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:ice_cream_app/screens/register_page.dart';
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFB0E5FE), // Updated background color
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Sing In",
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 50),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter Your Email",
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter Your Password",
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pinkAccent,
+                foregroundColor: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+              ),
+              child: const Text(
+                "Login",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 28,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterPage()),
+                );
+              },
+              child: Text(
+                "Don't have an account? Register",
+                style: TextStyle(
+                  fontFamily: 'Poppins', // Font-ka aad soo darsatay
+                  fontSize: 15,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

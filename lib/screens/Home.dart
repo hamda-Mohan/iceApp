9f071e0b628773c44bd8197d614f4818f842516e
RAHMA-SAID-afk <rahmasaid745@gmail.com>
2025-02-02 04:18:05 -0800

    Update Home.dart

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,1 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:ice_cream_app/screens/login.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFB0E5FE),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Spacer(),
+          Center(
+              child: Image.asset('assets/logo.png', height: 300)), // Logo Image
+          const SizedBox(height: 20),
+          const Center(
+            child: Text(
+              "Enjoy Your Ice Cream",
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.pink[100],
+              foregroundColor: Colors.white,
+              // primary: Colors.pinkAccent,
+              // onPrimary: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: const Text(
+              "Get Started",
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
